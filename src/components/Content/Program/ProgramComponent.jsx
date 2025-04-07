62f562c98a68ae121fd0681b67ddce6c9c60204d
carloozed <carlo@ozelot.ltd>
2025-04-07 12:45:59 +0200

    changes

--- src/components/Content/Program/ProgramComponent.jsx
+++ src/components/Content/Program/ProgramComponent.jsx
@@ -25,45 +25,45 @@ export default function ProgramComponent({ ...contentProps }) {
         activeNavigation === 'program' ? styles.open : ''
       }`}
     >
-      {/* <div className={styles.content}> */}
-      <div className={styles.titleContainer}>
-        <img src={programImage} onClick={() => setActiveNavigation('')} />
-        <img src={programImage} onClick={() => setActiveNavigation('')} />
-      </div>
-      <div className={styles.events}>
-        {programData.map((day, index) => (
-          <div className={styles.event} key={index}>
-            <div className={styles.leftSide}>
-              <div>
-                <h2>{day.date}</h2>
-              </div>
-            </div>
-            <div className={styles.rightSide}>
-              {day.blur && (
-                <div className={styles.overlay}>
-                  <h3>SOON</h3>
+      <div className={styles.content}>
+        <div className={styles.titleContainer}>
+          <img src={programImage} onClick={() => setActiveNavigation('')} />
+          <img src={programImage} onClick={() => setActiveNavigation('')} />
+        </div>
+        <div className={styles.events}>
+          {programData.map((day, index) => (
+            <div className={styles.event} key={index}>
+              <div className={styles.leftSide}>
+                <div>
+                  <h2>{day.date}</h2>
                 </div>
-              )}
-              <h3>{day.label}</h3>
-              <div>
-                {day.acts.map((act, index) => {
-                  return (
-                    <div key={index} className={styles.act}>
-                      <h4 className={styles.actName}>{act[0]}</h4>
-                      <h4>{act[1]}</h4>
-                    </div>
-                  );
-                })}
+              </div>
+              <div className={styles.rightSide}>
+                {day.blur && (
+                  <div className={styles.overlay}>
+                    <h3>SOON</h3>
+                  </div>
+                )}
+                <h3>{day.label}</h3>
+                <div>
+                  {day.acts.map((act, index) => {
+                    return (
+                      <div key={index} className={styles.act}>
+                        <h4 className={styles.actName}>{act[0]}</h4>
+                        <h4>{act[1]}</h4>
+                      </div>
+                    );
+                  })}
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-      </div>{' '}
-      <div className={styles.titleContainer}>
-        <img src={programImage} onClick={() => setActiveNavigation('')} />
-        <img src={programImage} onClick={() => setActiveNavigation('')} />
+          ))}
+        </div>{' '}
+        <div className={styles.titleContainer}>
+          <img src={programImage} onClick={() => setActiveNavigation('')} />
+          <img src={programImage} onClick={() => setActiveNavigation('')} />
+        </div>
       </div>
-      {/* </div> */}
     </div>
   );
 }
