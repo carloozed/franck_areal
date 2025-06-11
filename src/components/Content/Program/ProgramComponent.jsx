fde9a3a95804c32b352ed1ff4c699a55b59ded3e
carloozed <carlo@ozelot.ltd>
2025-06-11 16:33:20 +0200

    changes

--- src/components/Content/Program/ProgramComponent.jsx
+++ src/components/Content/Program/ProgramComponent.jsx
@@ -1,23 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 import styles from './Program.module.css';
 
-import { program, programDesktop } from './program';
-
 import programImage from '../../../assets/images/program_1.png';
+
+import programFinal from '../../../assets/images/program_final.png';
 
 export default function ProgramComponent({ ...contentProps }) {
   const { activeNavigation, setActiveNavigation } = contentProps;
-  const [isDesktop, setIsDesktop] = useState();
-  const [programData, setProgramData] = useState(program);
-
-  useEffect(() => {
-    window.innerWidth > 768 ? setIsDesktop(true) : setIsDesktop(false);
-  }, []);
-
-  useEffect(() => {
-    isDesktop ? setProgramData(programDesktop) : setProgramData(program);
-  }, [isDesktop]);
 
   return (
     <div
@@ -30,34 +20,8 @@ export default function ProgramComponent({ ...contentProps }) {
           <img src={programImage} onClick={() => setActiveNavigation('')} />
         </div>
         <div className={styles.events}>
-          {programData.map((day, index) => (
-            <div className={styles.event} key={index}>
-              <div className={styles.leftSide}>
-                <div>
-                  <h2>{day.date}</h2>
-                </div>
-              </div>
-              <div className={styles.rightSide}>
-                {/* {day.blur && (
-                  <div className={styles.overlay}>
-                    <h3>SOON</h3>
-                  </div>
-                )} */}
-                <h3>{day.label}</h3>
-                <div>
-                  {day.acts.map((act, index) => {
-                    return (
-                      <div key={index} className={styles.act}>
-                        <h4 className={styles.actName}>{act[0]}</h4>
-                        {/* <h4>{act[1]}</h4> */}
-                      </div>
-                    );
-                  })}
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>{' '}
+          <img src={programFinal} />
+        </div>
         <div className={styles.titleContainer}>
           <img src={programImage} onClick={() => setActiveNavigation('')} />
         </div>
